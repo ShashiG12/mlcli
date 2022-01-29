df99797ec940aade60922d6933f8535229e4bffb
ShashiG12 <34193029+ShashiG12@users.noreply.github.com>
2022-01-29 13:32:15 -0500

    updates

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
         parser.add_argument('modelType', choices=['linear', 'logistic'], default='linear',nargs='?', help='Model Type')
         parser.add_argument('-p', '--plot', help='set plot to show', action='store_true')
         parser.add_argument('-s', '--string', help='set string labels', action='store_true')
+        parser.add_argument('-c', '--column', help='choose label column (default is last column)', default=0)
 
         args = parser.parse_args()
-
-        model = mlModel(args.data, args.modelType, args.string, args.plot, .33)
+        print(args.column)
+        model = mlModel(args.data, args.modelType, args.string, args.plot, args.column, .33)
         print(f"Model Runtime: {round(time.time() - startTime, 2)} seconds")
 
         x = input('Exit?: ')
--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import sklearn
 import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -11,24 +10,39 @@ class mlModel:
     # modelType - type of model [linear]
     # stringLabels - whether labels are strings
     # testPercentage - percent of data to be used for testing
-    def __init__(self, dataFile, modelType, stringLabels, showPlot, testPercentage):
+    def __init__(self, dataFile, modelType, stringLabels, showPlot, labelColumn, testPercentage):
         self.dataFile = dataFile
+        self.df = pandas.read_csv(self.dataFile)
         self.modelType = modelType
         self.stringLabels = stringLabels
         self.showPlot = showPlot
+
+        if labelColumn == 0:
+            self.labelColumn = len(self.df.columns)
+        else:
+            self.labelColumn = labelColumn
+        # self.labelColumn = labelColumn
+
         self.testPercentage = testPercentage
         self.X_train, self.X_test, self.y_train, self.y_test = 0,0,0,0
 
+        print(self.df.head())
+
+        # check if the label column is made of strings
         if stringLabels:
             self.stringLabelToNum()
+        else:
+            self.normalLabels()
         self.play()
 
+    # convert string label column to nums and return corresponding dictionary
     def stringLabelToNum(self):
-        df = pandas.read_csv(self.dataFile)
-        print(df.head())
+        # df = pandas.read_csv(self.dataFile)
+        # print(df.head())
+
         dataSet = {}
         count = 0
-        df2 = df.iloc[:, -1].copy()
+        df2 = self.df.iloc[:, -1].copy()
         for count1, label in enumerate(df2):
             if label in dataSet:
                 df2.loc[count1] = dataSet[label]
@@ -37,10 +51,19 @@ class mlModel:
                 df2.loc[count1] = count
                 count += 1
         print(f'String to number mapping: {dataSet}')
-        X = df.iloc[:, :-1]
+        X = self.df.iloc[:, :-1]
         y = df2
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.testPercentage)
 
+    def normalLabels(self):
+        # df = pandas.read_csv(self.dataFile)
+        # print(df.head())
+
+        X = self.df.iloc[:, :-1]
+        y = self.df.iloc[:, -1].copy()
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.testPercentage)
+
+    # run correct ML model
     def play(self):
         if self.modelType == 'linear':
             self.linearModel()
@@ -53,15 +76,16 @@ class mlModel:
         print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
         self.plot(model)
 
+    def logisticModel(self):
+        model = LogisticRegression(multi_class='multinomial')
+        model.fit(self.X_train, self.y_train)
+        print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
+        self.plot(model)
+
+    # scatter plot the data with predictions
     def plot(self, model):
         if self.showPlot:
             y_pred = model.predict(self.X_test)
             plt.scatter(self.X_test.iloc[:,0], self.y_test)
             plt.plot(self.X_test.iloc[:,0], y_pred)
             plt.show()
-
-    def logisticModel(self):
-        model = LogisticRegression(multi_class='multinomial')
-        model.fit(self.X_train, self.y_train)
-        print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
-        self.plot(model)
