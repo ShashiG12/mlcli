263b92232f2e984e97072d6052fb8b1f3e94a86b
ShashiG12 <34193029+ShashiG12@users.noreply.github.com>
2022-01-29 10:54:40 -0500

    updates

--- main.py
+++ main.py
@@ -1,49 +1,26 @@
 import argparse
 import time
-import pandas
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from model import mlModel
-
-def preProcess(dataFile):
-    df = pandas.read_csv(dataFile)
-    print(df.head())
-
-def linearModel(dataFile):
-    df = pandas.read_csv(dataFile)
-    print(df.head())
-    dataSet = {}
-    count = 0
-    df2 = df.iloc[:, -1].copy()
-    for count1, label in enumerate(df2):
-        if label in dataSet:
-            df2.loc[count1] = dataSet[label]
-        else:
-            dataSet[label] = count
-            df2.loc[count1] = count
-            count+=1
-    print(f'String to number mapping: {dataSet}')
-    X = df.iloc[:,:-1]
-    y = df2
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    print(f'Accuracy: {model.score(X_test, y_test)}')
-
 
 if __name__ == '__main__':
     startTime = time.time()
-    parser = argparse.ArgumentParser(
-        description='CLI for Machine Learning Model generation')
-    parser.add_argument('data', help='csv file containing data')
-    parser.add_argument('modelType', choices=['linear', 'logistic'], default='linear',nargs='?', help='Model Type')
-    # parser.add_argument()
-    parser.add_argument('-s', '--string', help='set string labels', action='store_true')
-    args = parser.parse_args()
-    # linearModel(args.data)
-    model = mlModel(args.data, args.modelType, args.string, .33)
-    print(f"Runtime: {round(time.time() - startTime, 2)} seconds")
-    # test
+    x = ''
+    while x.lower() != 'y' and x.lower() != 'yes':
+        parser = argparse.ArgumentParser(
+            description='CLI for Machine Learning Model generation')
+        parser.add_argument('data', help='csv file containing data')
+        parser.add_argument('modelType', choices=['linear', 'logistic'], default='linear',nargs='?', help='Model Type')
+        parser.add_argument('-p', '--plot', help='set plot to show', action='store_true')
+        parser.add_argument('-s', '--string', help='set string labels', action='store_true')
+
+        args = parser.parse_args()
+
+        model = mlModel(args.data, args.modelType, args.string, args.plot, .33)
+        print(f"Model Runtime: {round(time.time() - startTime, 2)} seconds")
+
+        x = input('Exit?: ')
+
+    print(f"Script Runtime: {round(time.time() - startTime, 2)} seconds")
 
 
 
--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
 
 
 class mlModel:
@@ -10,12 +11,19 @@ class mlModel:
     # modelType - type of model [linear]
     # stringLabels - whether labels are strings
     # testPercentage - percent of data to be used for testing
-    def __init__(self, dataFile, modelType, stringLabels, testPercentage):
+    def __init__(self, dataFile, modelType, stringLabels, showPlot, testPercentage):
         self.dataFile = dataFile
         self.modelType = modelType
         self.stringLabels = stringLabels
+        self.showPlot = showPlot
         self.testPercentage = testPercentage
+        self.X_train, self.X_test, self.y_train, self.y_test = 0,0,0,0
 
+        if stringLabels:
+            self.stringLabelToNum()
+        self.play()
+
+    def stringLabelToNum(self):
         df = pandas.read_csv(self.dataFile)
         print(df.head())
         dataSet = {}
@@ -31,8 +39,7 @@ class mlModel:
         print(f'String to number mapping: {dataSet}')
         X = df.iloc[:, :-1]
         y = df2
-        self. X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.testPercentage)
-        self.play()
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.testPercentage)
 
     def play(self):
         if self.modelType == 'linear':
@@ -44,8 +51,17 @@ class mlModel:
         model = LinearRegression()
         model.fit(self.X_train, self.y_train)
         print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
+        self.plot(model)
+
+    def plot(self, model):
+        if self.showPlot:
+            y_pred = model.predict(self.X_test)
+            plt.scatter(self.X_test.iloc[:,0], self.y_test)
+            plt.plot(self.X_test.iloc[:,0], y_pred)
+            plt.show()
 
     def logisticModel(self):
         model = LogisticRegression(multi_class='multinomial')
         model.fit(self.X_train, self.y_train)
         print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
+        self.plot(model)
