8d8b7c0c4a9023816d58f80e4f1ca4403360f0ac
ShashiG12 <34193029+ShashiG12@users.noreply.github.com>
2022-01-30 18:50:05 -0500

    added prediction feature

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ from model import mlModel
 
 def main():
     startTime = time.time()
-    x = ''
-    while x.lower() != 'y' and x.lower() != 'yes':
-        parser = argparse.ArgumentParser(
-            description='CLI for Machine Learning Model generation')
-        parser.add_argument('data', help='csv file containing data')
-        parser.add_argument('modelType', choices=['linear', 'logistic', 'svm'], default='linear',nargs='?', help='Model Type')
-        parser.add_argument('-p', '--plot', help='set plot to show', action='store_true')
-        parser.add_argument('-s', '--string', help='set string labels', action='store_true')
-        parser.add_argument('-c', '--column', help='choose label column (default is last column)', default=0)
-        parser.add_argument('-t', '--test', help='choose the size of test set (default=.33)', type=float, default=.33)
 
-        args = parser.parse_args()
-        print(args.column)
-        model = mlModel(args.data, args.modelType, args.string, args.plot, args.column, args.test)
-        print(f"Model Runtime: {round(time.time() - startTime, 2)} seconds")
+    parser = argparse.ArgumentParser(
+        description='CLI for Machine Learning Model generation')
+    parser.add_argument('data', help='csv file containing data')
+    parser.add_argument('modelType', choices=['linear', 'logistic', 'svm'], default='linear',nargs='?', help='Model Type')
+    parser.add_argument('-p', '--plot', help='set plot to show', action='store_true')
+    parser.add_argument('-s', '--string', help='set string labels', action='store_true')
+    parser.add_argument('-c', '--column', help='choose label column (default is last column)', default=0)
+    parser.add_argument('-t', '--test', help='choose the size of test set (default=.33)', type=float, default=.33)
+    parser.add_argument('--predict', help='allow prediction of new sample with model', action='store_true')
 
-        x = input('Exit?: ')
+    args = parser.parse_args()
+    print(args.column)
+    model = mlModel(args.data, args.modelType, args.string, args.plot, args.column, args.test, args.predict)
 
     print(f"Script Runtime: {round(time.time() - startTime, 2)} seconds")
+    print(f"Script Runtime: {round(time.time() - startTime, 2)} seconds")
+
 
 if __name__ == '__main__':
     main()
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+import time
 
 
 class mlModel:
@@ -11,13 +12,18 @@ class mlModel:
     # modelType - type of model [linear]
     # stringLabels - whether labels are strings
     # testPercentage - percent of data to be used for testing
-    def __init__(self, dataFile, modelType, stringLabels, showPlot, labelColumn, testPercentage):
+    def __init__(self, dataFile, modelType, stringLabels, showPlot, labelColumn, testPercentage, isPredict):
+        startTime = time.time()
         self.dataFile = dataFile
         self.df = pandas.read_csv(self.dataFile)
         self.modelType = modelType
         self.stringLabels = stringLabels
         self.showPlot = showPlot
+        self.isPredict = isPredict
 
+        self.model = None
+
+        # check for default last column of features
         if labelColumn == 0:
             self.labelColumn = len(self.df.columns)
         else:
@@ -35,12 +41,12 @@ class mlModel:
         else:
             self.normalLabels()
         self.play()
+        print(f"Model Runtime: {round(time.time() - startTime, 2)} seconds")
+        self.predict()
 
     # convert string label column to nums and return corresponding dictionary
     def stringLabelToNum(self):
-        # df = pandas.read_csv(self.dataFile)
-        # print(df.head())
-
+        # converts any number of strings labels to nums using a dictionary
         dataSet = {}
         count = 0
         df2 = self.df.iloc[:, -1].copy()
@@ -57,9 +63,6 @@ class mlModel:
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.testPercentage)
 
     def normalLabels(self):
-        # df = pandas.read_csv(self.dataFile)
-        # print(df.head())
-
         X = self.df.iloc[:, :-1]
         y = self.df.iloc[:, -1].copy()
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=self.testPercentage)
@@ -74,27 +77,39 @@ class mlModel:
             self.svmModel()
 
     def linearModel(self):
-        model = LinearRegression()
-        model.fit(self.X_train, self.y_train)
-        print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
-        self.plot(model)
+        self.model = LinearRegression()
+        self.afterModel()
 
     def logisticModel(self):
-        model = LogisticRegression(multi_class='multinomial')
-        model.fit(self.X_train, self.y_train)
-        print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
-        self.plot(model)
+        self.model = LogisticRegression(multi_class='multinomial')
+        self.afterModel()
 
     def svmModel(self):
-        model = SVC(gamma='auto')
-        model.fit(self.X_train, self.y_train)
-        print(f'Accuracy: {model.score(self.X_test, self.y_test)}')
-        self.plot(model)
+        self.model = SVC(gamma='auto')
+        self.afterModel()
+
+    def afterModel(self):
+        self.model.fit(self.X_train, self.y_train)
+        print(f'Accuracy: {self.model.score(self.X_test, self.y_test)}')
+        self.plot()
 
     # scatter plot the data with predictions
-    def plot(self, model):
+    def plot(self):
         if self.showPlot:
-            y_pred = model.predict(self.X_test)
+            y_pred = self.model.predict(self.X_test)
             plt.scatter(self.X_test.iloc[:,0], self.y_test)
             plt.plot(self.X_test.iloc[:,0], y_pred)
             plt.show()
+
+    def predict(self):
+        if self.isPredict:
+            y = [[]]
+            # x = ''
+            # while x.lower() != 'q' and x.lower() != 'quit':
+            #     for column in self.df.columns():
+            #         x = input('Input: ')
+            #         self.model.predict(x)
+            for i, column in enumerate(self.df.columns):
+                y[0].append(float(input(f'{self.df.columns[i]}: ')))
+            y[0].pop(4)
+            print(self.model.predict(y)[0])
